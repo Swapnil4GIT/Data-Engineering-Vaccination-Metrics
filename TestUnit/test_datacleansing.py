cf0574d2c989935a417a507ccc0436cbc891d4e0
Swapnil4GIT <100533909+Swapnil4GIT@users.noreply.github.com>
2023-04-08 18:10:08 +0530

    Added more unit tests

--- TestUnit/test_datacleansing.py
+++ TestUnit/test_datacleansing.py
@@ -2,6 +2,7 @@ import pytest
 import os
 import sys
 import pandas as pd
+import numpy as np
 from typing import Callable, NoReturn
 
 sys.path.insert(0, '/Volumes/E-Study/Github/Data-Engineering-Vaccination-Metrics')
@@ -75,6 +76,75 @@ def test_aus_dataset_columns(aus_dataset_columns: Callable) -> NoReturn:
                         'Date of Vaccination']
     assert expected_columns == aus_dataset_columns
 
+
+@pytest.fixture
+def ind_dataset_columns_dtype() -> dict[str, np.dtype]:
+    """
+    Get dataset column data types and validate the same.
+    """
+    fname = filepath + 'IND.csv'
+    df = pd.read_csv(fname)
+    df_ind_dataset_columns_dtypes = df.dtypes.to_dict()
+    return df_ind_dataset_columns_dtypes
+
+def test_ind_dataset_columns_dtype(ind_dataset_columns_dtype: Callable) -> NoReturn:
+    """
+    Verify the datatypes of the input dataframe are as expected.
+    """
+    expected_data_types = {
+        'ID' : np.dtype('int64'),
+        'Name' : np.dtype('O'),
+        'DOB' : np.dtype('O'),
+        'VaccinationType' : np.dtype('O'),
+        'VaccinationDate' : np.dtype('O'),
+        'Free or Paid' : np.dtype('O')
+    }
+    assert expected_data_types == ind_dataset_columns_dtype
+
+@pytest.fixture
+def usa_dataset_columns_dtype() -> dict[str, np.dtype]:
+    """
+    Get dataset column data types and validate the same.
+    """
+    fname = filepath + 'USA.csv'
+    df = pd.read_csv(fname)
+    df_usa_dataset_columns_dtypes = df.dtypes.to_dict()
+    return df_usa_dataset_columns_dtypes
+
+def test_usa_dataset_columns_dtype(usa_dataset_columns_dtype: Callable) -> NoReturn:
+    """
+    Verify the datatypes of the input dataframe are as expected.
+    """
+    expected_data_types = {
+        'ID' : np.dtype('int64'),
+        'Name' : np.dtype('O'),
+        'VaccinationType' : np.dtype('O'),
+        'VaccinationDate' : np.dtype('int64')
+    }
+    assert expected_data_types == usa_dataset_columns_dtype
+
+@pytest.fixture
+def aus_dataset_columns_dtype() -> dict[str, np.dtype]:
+    """
+    Get dataset column data types and validate the same.
+    """
+    fname = filepath + 'AUS.xlsx'
+    df = pd.read_excel(fname)
+    df_aus_dataset_columns_dtypes = df.dtypes.to_dict()
+    return df_aus_dataset_columns_dtypes
+
+def test_aus_dataset_columns_dtype(aus_dataset_columns_dtype: Callable) -> NoReturn:
+    """
+    Verify the datatypes of the input dataframe are as expected.
+    """
+    expected_data_types = {
+        'Unique ID' : np.dtype('int64'),
+        'Patient Name' : np.dtype('O'),
+        'Vaccine Type' : np.dtype('O'),
+        'Date of Birth' : np.dtype('datetime64[ns]'),
+        'Date of Vaccination' : np.dtype('O')
+    }
+    assert expected_data_types == aus_dataset_columns_dtype
 
 def test_datacleansing():
     return_code = dataclean.job_Datacleansing(logger)
